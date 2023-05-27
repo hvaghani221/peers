7704f0bea22c7925e9b190cf409b3f9d3e00c1d3
hvaghani221 <harshitvaghani221@gmail.com>
2023-05-27 18:25:29 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/hvaghani221/peers
+
+go 1.20
+
+require (
+	github.com/perlin-network/noise v1.1.3
+	github.com/pion/ice/v2 v2.3.6
+	github.com/pion/randutil v0.1.0
+)
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/oasislabs/ed25519 v0.0.0-20200302143042-29f6767a7c3e // indirect
+	github.com/pion/dtls/v2 v2.2.7 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.7 // indirect
+	github.com/pion/stun v0.6.0 // indirect
+	github.com/pion/transport/v2 v2.2.1 // indirect
+	github.com/pion/turn/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
+	go.uber.org/atomic v1.5.1 // indirect
+	go.uber.org/multierr v1.3.0 // indirect
+	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
+	go.uber.org/zap v1.13.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/tools v0.0.1-2019.2.3 // indirect
+)
---
+++ ice/ice.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	ice "github.com/pion/ice/v2"
+)
+
+type ICE struct {
+	Username   string   `json:"username"`
+	Password   string   `json:"password"`
+	Candidates []string `json:"candidates"`
+	mutex      sync.Mutex
+}
+
+func (i *ICE) Add(c ice.Candidate) {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+
+	i.Candidates = append(i.Candidates, c.Marshal())
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("ice send/receive")
+	}
+
+	var candidates *ICE = &ICE{}
+	iceAgent, err := ice.NewAgent(&ice.AgentConfig{
+		NetworkTypes: []ice.NetworkType{ice.NetworkTypeUDP4},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// When we have gathered a new ICE Candidate send it to the remote peer
+	if err = iceAgent.OnCandidate(func(c ice.Candidate) {
+		if c == nil {
+			return
+		}
+		candidates.Add(c)
+		fmt.Printf("candidate: (%s)\n", c.Marshal())
+	}); err != nil {
+		panic(err)
+	}
+
+	// When ICE Connection state has change print to stdout
+	if err = iceAgent.OnConnectionStateChange(func(c ice.ConnectionState) {
+		fmt.Printf("ICE Connection State has changed: %s\n", c.String())
+	}); err != nil {
+		panic(err)
+	}
+
+	// Get the local auth details and send to remote peer
+	candidates.Username, candidates.Password, err = iceAgent.GetLocalUserCredentials()
+	if err != nil {
+		panic(err)
+	}
+
+	if err = iceAgent.GatherCandidates(); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Millisecond * 100)
+	remoteCandidates, err := encodeAndDecode(candidates)
+	if err != nil {
+		panic(err)
+	}
+
+	index := 0
+	if os.Args[1] == "send" {
+		index = 1
+	}
+	rc, err := ice.UnmarshalCandidate(remoteCandidates.Candidates[index])
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Received request for: ")
+	if err := iceAgent.AddRemoteCandidate(rc); err != nil {
+		panic(err)
+	}
+
+	var exec func(*ice.Agent, string, string) error
+
+	if os.Args[1] == "send" {
+		exec = Dial
+	} else {
+		exec = Accept
+	}
+	if err := exec(iceAgent, remoteCandidates.Username, remoteCandidates.Password); err != nil {
+		panic(err)
+	}
+}
+
+func Dial(iceAgent *ice.Agent, username, password string) error {
+	conn, err := iceAgent.Dial(context.TODO(), username, password)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Started communication with ", conn.RemoteAddr())
+	go func() {
+		for i := 0; i < 3; i++ {
+			msg := fmt.Sprintf("hello %d", i)
+			fmt.Fprint(conn, msg)
+			fmt.Println("Sent: ", msg)
+		}
+	}()
+
+	buf := make([]byte, 1500)
+	for i := 0; i < 3; i++ {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Received: '%s'\n", string(buf[:n]))
+	}
+	return nil
+}
+
+func Accept(iceAgent *ice.Agent, username, password string) error {
+	conn, err := iceAgent.Accept(context.TODO(), username, password)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Started communication with ", conn.RemoteAddr())
+	go func() {
+		for i := 0; i < 3; i++ {
+			msg := fmt.Sprintf("world %d", i)
+			fmt.Fprint(conn, msg)
+			fmt.Println("Sent: ", msg)
+		}
+	}()
+
+	buf := make([]byte, 1500)
+	for i := 0; i < 3; i++ {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Received: '%s'\n", string(buf[:n]))
+	}
+	return nil
+}
+
+func encodeAndDecode(candidates *ICE) (*ICE, error) {
+	file, err := os.Create(os.Args[1] + ".json")
+	if err != nil {
+		return nil, err
+	}
+	if err := json.NewEncoder(file).Encode(&candidates); err != nil {
+		return nil, err
+	}
+	var filename string
+	if os.Args[1] == "send" {
+		filename = "receive.json"
+	} else {
+		filename = "send.json"
+	}
+	file.Close()
+
+	fmt.Printf("Waiting for file[%s]...", filename)
+
+	for {
+		file, err = os.Open(filename)
+		if err == nil {
+			fmt.Println("File detected")
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	remoteCandidates := &ICE{}
+	if err := json.NewDecoder(file).Decode(remoteCandidates); err != nil {
+		return nil, err
+	}
+
+	return remoteCandidates, nil
+}
